03535a48995e4b4b9d15c39b16cd3792ca31326a
stormymayday <stormymayday7@gmail.com>
2024-08-23 22:09:45 -0700

    Arrays

---
+++ src/tutorials/arraysAndObjects.ts
@@ -0,0 +1,19 @@
+// Array of numbers:
+let prices: number[] = [100, 75, 42, 34];
+
+prices.push(20);
+// prices.push('hello');
+
+let array: (string | boolean)[] = ["apple", true];
+
+// Create an array temperatures of type number[] and assign it some values. Then, try to add a string value to it.
+let temperatures: number[] = [1, 2, 3];
+// temperatures.push('hello');
+
+// Create an array colors of type string[] and assign it some values. Then, try to add a boolean value to it.
+let colors: string[] = ["red", "green", "blue"];
+// colors.unshift(false);
+
+//Create an array mixedArray of type (number | string)[] and assign it some values. Then, try to add a boolean value to it.
+let mixedArray: (number | string)[] = [1, "hello"];
+// mixedArray.push(true);

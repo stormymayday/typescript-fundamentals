ac8b97b3b1a9a0dbcc96553776e186d00647c2c8
stormymayday <stormymayday7@gmail.com>
2024-08-24 18:08:31 -0700

    Function Returns

--- src/tutorials/functionParamsAndReturns.ts
+++ src/tutorials/functionParamsAndReturns.ts
@@ -5,3 +5,16 @@ function sayHi(name: string) {
 
 sayHi("John");
 // sayHi(3);
+
+// Function Returns
+function calculateDiscount(price: number): number {
+    // const hasDiscount = true;
+    // if (hasDiscount) {
+    //     return "Discount Applied";
+    // }
+
+    return price * 0.9;
+}
+const finalPrice = calculateDiscount(200);
+
+console.log(finalPrice);

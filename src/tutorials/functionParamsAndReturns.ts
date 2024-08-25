5923ecc9828c92af2f04a31714ad0fc59270aa53
stormymayday <stormymayday7@gmail.com>
2024-08-24 18:12:27 -0700

    'any' example

--- src/tutorials/functionParamsAndReturns.ts
+++ src/tutorials/functionParamsAndReturns.ts
@@ -18,3 +18,13 @@ function calculateDiscount(price: number): number {
 const finalPrice = calculateDiscount(200);
 
 console.log(finalPrice);
+
+// "any" example
+function addThree(number: any) {
+    let anotherNumber: number = 3;
+
+    return number + anotherNumber;
+}
+const result = addThree(3);
+const someValue = result;
+// someValue.myMethod();

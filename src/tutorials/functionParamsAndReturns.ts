73e9ac647ca72d3491ba358945df5224f60b566a
stormymayday <stormymayday7@gmail.com>
2024-08-24 18:20:26 -0700

    Function Return and Params Challenge

--- src/tutorials/functionParamsAndReturns.ts
+++ src/tutorials/functionParamsAndReturns.ts
@@ -28,3 +28,24 @@ function addThree(number: any) {
 const result = addThree(3);
 const someValue = result;
 // someValue.myMethod();
+
+// Function Params and Returns Challenge
+// Create a new array of names.
+// Write a new function that checks if a name is in your array. This function should take a name as a parameter and return a boolean.
+// Use this function to check if various names are in your array and log the results.
+
+let names: string[] = ["john", "peter"];
+
+function checkName(name: string): boolean {
+    for (let i = 0; i < names.length; i++) {
+        if (name === names[i]) {
+            return true;
+        }
+    }
+    return false;
+
+    // return names.includes(name);
+}
+
+console.log(checkName("kevin"));
+console.log(checkName("peter"));

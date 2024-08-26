782085af3df9f0d0113fe7141507cc976d8d0577
stormymayday <stormymayday7@gmail.com>
2024-08-26 00:48:33 -0700

    Using Union Types as Function Parameters

--- src/tutorials/functionsOptionalDefaultRestParameters.ts
+++ src/tutorials/functionsOptionalDefaultRestParameters.ts
@@ -39,3 +39,22 @@ function logMessage(message: string) {
 }
 
 logMessage("Hello, Typescript");
+
+// Functions - Using Union Types as Function Parameters
+// Challenge
+// Your task is to create a function named processInput that accepts a parameter of a union type string | number. The function should behave as follows:
+// - If the input is of type number, the function should multiply the number by 2 and log the result to the console.
+// - If the input is of type string, the function should convert the string to uppercase and log the result to the console.
+
+function processInput(input: string | number) {
+    if (typeof input === "number") {
+        console.log(input * 2);
+    }
+
+    if (typeof input === "string") {
+        console.log(input.toUpperCase());
+    }
+}
+
+processInput(10);
+processInput("hello");

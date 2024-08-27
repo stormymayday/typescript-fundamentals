bc95906583a4274d464f24fe3ea4a928d6f422d3
stormymayday <stormymayday7@gmail.com>
2024-08-26 17:31:02 -0700

    Functions - Using Objects as Function Parameters (Naming an object)

--- src/tutorials/objectsAsFunctionParameters.ts
+++ src/tutorials/objectsAsFunctionParameters.ts
@@ -1,9 +1,22 @@
 // Using 'destructuring'
-function createEmployee({ id }: { id: number }): {
+// function createEmployee({ id }: { id: number }): {
+//     id: number;
+//     isActive: boolean;
+// } {
+//     return { id, isActive: id % 2 === 0 };
+// }
+
+// const first = createEmployee({ id: 1 });
+// const second = createEmployee({ id: 2 });
+
+// console.log(first, second);
+
+// Naming an object
+function createEmployee(employee: { id: number }): {
     id: number;
     isActive: boolean;
 } {
-    return { id, isActive: id % 2 === 0 };
+    return { id: employee.id, isActive: employee.id % 2 === 0 };
 }
 
 const first = createEmployee({ id: 1 });

bd4996eac1def23f640a7424fed8690ac9fb6e2b
stormymayday <stormymayday7@gmail.com>
2024-08-26 20:38:46 -0700

    Using Objects as Function Parameters - challenge

--- src/tutorials/objectsAsFunctionParameters.ts
+++ src/tutorials/objectsAsFunctionParameters.ts
@@ -38,3 +38,29 @@ createStudent(newStudent);
 
 // Gotcha - Excess Property Checks:
 // createStudent({ id: 1, name: "bob", email: "bobo@gmail.com" });
+
+// ## Challenge
+// Your task is to create a function named processData that accepts two parameters:
+// - The first parameter, input, should be a union type that can be either a string or a number.
+// - The second parameter, config, should be an object with a reverse property of type boolean, by default it "reverse" should be false
+// The function should behave as follows:
+// - If input is of type number, the function should return the square of the number.
+// - If input is of type string, the function should return the string in uppercase.
+// - If the reverse property on the config object is true, and input is a string, the function should return the reversed string in uppercase.
+
+function processData(
+    input: string | number,
+    config: { reverse: boolean } = { reverse: false }
+): string | number {
+    if (typeof input === "number") {
+        return input * input;
+    } else {
+        return config.reverse
+            ? input.toUpperCase().split("").reverse().join("")
+            : input.toUpperCase();
+    }
+}
+
+console.log(processData(2));
+console.log(processData("hello", { reverse: false }));
+console.log(processData("hello", { reverse: true }));

d9b525f4da6cf5dd562b98044b432245b5567084
stormymayday <stormymayday7@gmail.com>
2024-08-26 21:44:20 -0700

    Type Alias challenge

--- src/tutorials/typeAlias.ts
+++ src/tutorials/typeAlias.ts
@@ -35,3 +35,50 @@ function setTheme(t: Theme) {
     theme = t;
 }
 setTheme("dark");
+
+// ## Challenge
+
+// - Define the Employee type: Create a type Employee with properties id (number), name (string), and department (string).
+type Employee = { id: number; name: string; department: string };
+
+// - Define the Manager type: Create a type Manager with properties id (number), name (string), and employees (an array of Employee).
+type Manager = { id: number; name: string; employees: Employee[] };
+
+// - Create a Union Type: Define a type Staff that is a union of Employee and Manager.
+type Staff = Employee | Manager;
+
+// - Create the printStaffDetails function: This function should accept a parameter of type Staff. Inside the function, use a type guard to check if the 'employees' property exists in the passed object. If it does, print a message indicating that the person is a manager and the number of employees they manage. If it doesn't, print a message indicating that the person is an employee and the department they belong to.
+function printStaffDetails(staff: Staff): void {
+    if ("employees" in staff) {
+        // console.log(`Manager`);
+        console.log(
+            `${staff.name} is a manager and manages ${staff.employees.length} employees.`
+        );
+    } else {
+        console.log(
+            `${staff.name} is an employee and works in ${staff.department} department.`
+        );
+    }
+}
+
+// - Create Employee and Manager objects: Create two Employee objects. One named alice and second named steve. Also create a Manager object named bob who manages alice and steve.
+const alice: Employee = {
+    id: 1,
+    name: "alice",
+    department: "sales",
+};
+const steve: Employee = {
+    id: 2,
+    name: "steve",
+    department: "hr",
+};
+const bob: Manager = {
+    id: 3,
+    name: "bobo",
+    employees: [alice, steve],
+};
+
+// - Test the function: Call the printStaffDetails function with alice and bob as arguments and verify the output.
+printStaffDetails(alice);
+printStaffDetails(steve);
+printStaffDetails(bob);

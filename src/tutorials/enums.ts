7e341893c87ccfd7d01a6005d49053f756012f86
stormymayday <stormymayday7@gmail.com>
2024-08-29 16:01:30 -0700

    Tuples and Enum challenge

--- src/tutorials/enums.ts
+++ src/tutorials/enums.ts
@@ -26,3 +26,34 @@ function getServerResponse(): ServerResponse {
 
 const response: ServerResponse = getServerResponse();
 // console.log(response);
+
+// Challenge
+
+// Define an enum named UserRole with members Admin, Manager, and Employee.
+enum UserRole {
+    Admin,
+    Manager,
+    Employee,
+}
+
+// Define a type alias named User with properties id (number), name (string), role (UserRole), and contact (a tuple with two elements: email as string and phone as string).
+type User = {
+    id: number;
+    name: string;
+    role: UserRole;
+    contact: [string, string];
+};
+// Define a function named createUser that takes a User object as its parameter and returns a User object.
+function createUser(user: User): User {
+    return user;
+}
+// Call the createUser function with an object that matches the User type, store the result in a variable, and log the variable to the console.
+
+const john: User = createUser({
+    id: 1,
+    name: "john",
+    role: UserRole.Admin,
+    contact: ["john@gmail.com", "1234567890"],
+});
+
+console.log(john);

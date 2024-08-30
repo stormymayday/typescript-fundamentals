6814f2bc9a48b94fb97679079d301e867c2bd84d
stormymayday <stormymayday7@gmail.com>
2024-08-29 20:20:19 -0700

    'Truthy'/'Falsy' guard

--- src/tutorials/typeGuardingPart1.ts
+++ src/tutorials/typeGuardingPart1.ts
@@ -75,3 +75,30 @@ function makeSound(animal: Animal) {
 
 makeSound(cat1);
 makeSound(dog1);
+
+// Challenge - "Truthy"/"Falsy" guard
+
+// In TypeScript, "Truthy"/"Falsy" guard is a simple check for a truthy or falsy value
+
+// Define a function named printLength that takes one parameter str which can be of type string, null, or undefined.
+
+// Inside the function, use an if statement to check if str is truthy. In JavaScript and TypeScript, a truthy value is a value that is considered true when encountered in a Boolean context. All values are truthy unless they are defined as falsy (i.e., except for false, 0, -0, 0n, "", null, undefined, and NaN).
+
+// If str is truthy, it means it's a string (since null and undefined are falsy). In this case, log the length of str using the length property of the string.
+
+// If str is not truthy (i.e., it's either null or undefined), log the string 'No string provided'.
+
+function printLength(str: string | null | undefined): void {
+    if (str) {
+        console.log(str.length);
+    } else {
+        console.log(`No string provided`);
+    }
+}
+
+// Now you can call the printLength function with a string, null, or undefined as the argument. The function will print the length of the string if a string is provided, or 'No string provided' otherwise.
+
+printLength(`hello`);
+printLength("");
+printLength(null);
+printLength(undefined);

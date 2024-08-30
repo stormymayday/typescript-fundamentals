bb97a7c2f8e9d6bc158ed9f6bb492bc4dab7c901
stormymayday <stormymayday7@gmail.com>
2024-08-29 20:13:44 -0700

    Equality Narrowing

--- src/tutorials/typeGuardingPart1.ts
+++ src/tutorials/typeGuardingPart1.ts
@@ -1,3 +1,4 @@
+// Challenge - "typeof" guard
 type ValueType = string | number | boolean;
 
 let value: ValueType;
@@ -24,3 +25,53 @@ function checkValue(value: ValueType): void {
 // Finally, call the checkValue function with value as the argument.
 
 checkValue(value);
+
+// Equality Narrowing
+
+// In TypeScript, equality narrowing is a form of type narrowing that occurs when you use equality checks like === or !== in your code
+
+type Dog = { type: "dog"; name: string; bark: () => void };
+type Cat = { type: "cat"; name: string; meow: () => void };
+type Animal = Dog | Cat;
+
+const cat1: Cat = {
+    type: "cat",
+    name: "spot",
+    meow() {
+        console.log(`${this.type} named ${this.name} meows!`);
+    },
+};
+
+const dog1: Dog = {
+    type: "dog",
+    name: "sparky",
+    bark() {
+        console.log(`${this.type} named ${this.name} barks!`);
+    },
+};
+
+// Define a function named makeSound that takes one parameter animal of type Animal.
+
+// Inside the function, use an if statement to check if animal.type is 'dog'.
+
+// If animal.type is 'dog', TypeScript knows that animal is a Dog in this block. In this case, call the bark method of animal.
+
+// If animal.type is not 'dog', TypeScript knows that animal is a Cat in the else block. In this case, call the meow method of animal.
+
+// Now you can call the makeSound function with an Animal as the argument. The function will call the appropriate method (bark or meow) depending on the type of the animal.
+function makeSound(animal: Animal) {
+    // if ("bark" in animal) {
+    //     animal.bark();
+    // } else {
+    //     animal.meow();
+    // }
+
+    if (animal.type === "dog") {
+        animal.bark();
+    } else {
+        animal.meow();
+    }
+}
+
+makeSound(cat1);
+makeSound(dog1);

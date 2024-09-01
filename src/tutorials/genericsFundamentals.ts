2e49fc7f7c6c986457ddb8c06c3e185cc9d67748
stormymayday <stormymayday7@gmail.com>
2024-08-31 22:36:35 -0700

    Type Constraints 2

--- src/tutorials/genericsFundamentals.ts
+++ src/tutorials/genericsFundamentals.ts
@@ -88,3 +88,42 @@ function processValue<T extends number | string>(value: T): T {
 processValue("hello");
 processValue(12);
 // processValue(true);
+
+// Type Constraints 2
+type Car = {
+    brand: string;
+    model: string;
+};
+
+const car: Car = {
+    brand: "ford",
+    model: "mustang",
+};
+
+type Product = {
+    name: string;
+    price: number;
+};
+
+const product: Product = {
+    name: "shoes",
+    price: 1.99,
+};
+
+type Student = {
+    name: string;
+    age: number;
+};
+
+const student: Student = {
+    name: "peter",
+    age: 20,
+};
+
+function printName<T extends { name: string }>(input: T): void {
+    console.log(input.name);
+}
+
+printName(student);
+printName(product);
+// printName(car);

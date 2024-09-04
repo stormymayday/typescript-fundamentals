9d7726298b09ef0565654ffb061d075ff3de7ce2
stormymayday <stormymayday7@gmail.com>
2024-09-03 20:21:22 -0700

    Implementing a Class Interface

--- src/tutorials/classes.ts
+++ src/tutorials/classes.ts
@@ -15,3 +15,20 @@ const deepWork = new Book("Deep Work", "Cal Newport");
 console.log(deepWork.info);
 deepWork.checkOut = true;
 console.log(deepWork);
+
+// Implementing a Class Interface
+interface IPerson {
+    name: string;
+    age: number;
+    greet(): void;
+}
+
+class Person implements IPerson {
+    constructor(public name: string, public age: number) {}
+    greet(): void {
+        console.log(`My name is ${this.name} and I am ${this.age} years old`);
+    }
+}
+
+const hipster = new Person("shakeandbake", 100);
+hipster.greet();

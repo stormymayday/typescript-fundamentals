f4b8008d4e00d2541135156557902d3f4d4d9d6b
stormymayday <stormymayday7@gmail.com>
2024-09-03 20:16:33 -0700

    Getters and Setters

--- src/tutorials/classes.ts
+++ src/tutorials/classes.ts
@@ -1,6 +1,17 @@
 class Book {
     private checkedOut: boolean = false;
     constructor(public readonly title: string, public author: string) {}
+
+    get info() {
+        return `${this.title} by ${this.author}`;
+    }
+
+    set checkOut(checkedOut: boolean) {
+        this.checkedOut = checkedOut;
+    }
 }
 
 const deepWork = new Book("Deep Work", "Cal Newport");
+console.log(deepWork.info);
+deepWork.checkOut = true;
+console.log(deepWork);
